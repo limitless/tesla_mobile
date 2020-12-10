5af789cf0b11c2228b49c92cd2ac7b39a669092e
neetmangat <neetmangat@gmail.com>
2020-12-10 02:07:04 -0600

    Added custom buttons

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import { Text, View, ImageBackground } from 'react-native';
 import styles from './style';
+import StyledButton from '../StyledButton';
 
 const CarItem = (props) => {
     return(
         <View style={styles.carContainer}>
 
-        <ImageBackground 
-            source={require('../../assets/images/ModelS.jpeg')} 
-            style={styles.image}
-        />
+            <ImageBackground 
+                source={require('../../assets/images/ModelS.jpeg')} 
+                style={styles.image}
+            />
 
-        <View style={styles.titles}>
-            <Text style={styles.title}>Model S</Text>
-            <Text style={styles.subtitle}>Starting at $69,420</Text>
+            <View style={styles.titles}>
+                <Text style={styles.title}>Model S</Text>
+                <Text style={styles.subtitle}>Starting at $69,420</Text>
+            </View>
+
+            <StyledButton 
+                type='primary' 
+                content={'Custom Order'} 
+                onPress={() => {
+                    console.warn('Custom Order was Pressed');
+                }} 
+            />
+
+            <StyledButton 
+                type='secondary' 
+                content={'Existing Inventory'} 
+                onPress={() => {
+                    console.warn('Existing Inventory was Pressed');
+                }} 
+            />
+            
         </View>
-      </View>
     );
 };
 
